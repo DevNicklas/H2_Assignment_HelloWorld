e5a4ac241cad94ca096fb251fd745d04abfde81a
Nicklas Lindegaard Martlev Gustavsen <nicklasgustavsen75@gmail.com>
2024-04-15 09:50:10 +0200

    Finished project

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
-alert("Hello world");
+// Alert with "Hello world" and add onclick property to button, after the DOM's content is loaded.
+document.addEventListener("DOMContentLoaded", function() {
+    alert("Hello world");
+    document.getElementById("sayHelloBtn").onclick = sayHello;
+})
 
+// Change paragraph 
 function sayHello() {
     document.getElementById("text").innerHTML = "Hello world";
 }
